5d95bfd84685ce76b689fd33fc7943d98facf544
darce-radic <132355796+darce-radic@users.noreply.github.com>
2024-12-30 00:07:30 +1100

    Update safe_constants.py

--- safe_constants.py
+++ safe_constants.py
@@ -1,14 +1,14 @@
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly", "https://www.googleapis.com/auth/userinfo.email", "openid"]
 
-PROJECT_ID = "kodosh"
+PROJECT_ID = "solarprojectss"
 AUTH_URI = "https://accounts.google.com/o/oauth2/auth"
 TOKEN_URI = "https://oauth2.googleapis.com/token"
 AUTH_PROVIDER_X509_CERT_URL = "https://www.googleapis.com/oauth2/v1/certs"
 
 MAX_CHARACTER_LENGTH_EMAIL = 12000
 
-MAIN_REDIRECT_URI = 'https://kodosh.streamlit.app/'
+MAIN_REDIRECT_URI = 'https://kodosh.streamlit.app'
 # MAIN_REDIRECT_URI = 'http://localhost:8080/'
 
-ALL_REDIRECT_URIS = ["http://localhost:8080/", 'https://kodosh.streamlit.app/']
-ALL_JAVASCRIPT_ORIGINS = ["http://localhost:8080", 'https://kodosh.streamlit.app/']
+ALL_REDIRECT_URIS = ["http://localhost:8080/", 'https://kodosh.streamlit.app']
+ALL_JAVASCRIPT_ORIGINS = ["http://localhost:8080", 'https://kodosh.streamlit.app']
