5c5dea0694b604b0daa83f55cdc525257de0ca87
darce-radic <132355796+darce-radic@users.noreply.github.com>
2024-12-29 21:31:04 +1100

    initial

---
+++ main.py
@@ -0,0 +1,301 @@
+import streamlit as st
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from openai import OpenAI
+from tqdm.auto import tqdm
+from pinecone import Pinecone
+import html  # To escape special characters
+import json
+
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+from rag_agent import RagAgent
+from pinecone_utility import PineconeUtility
+from utility import authorize_gmail_api, authenticate_user
+
+# PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+
+pc = Pinecone(api_key=PINECONE_API_KEY)
+client = OpenAI()
+
+index = pc.Index("mails") # server index
+rag_agent = RagAgent(index)
+pinecone_utility = PineconeUtility(index)    
+
+
+MAX_EMAILS = 100 # TODO INCREASE AFTER TESTING
+K_MAILS_TO_RETURN = 10
+
+
+if "user_email" in st.session_state and st.session_state.user_email is not None:
+    # st.title(f"Hello {st.session_state.user_email}")
+    st.title(f"Hello ...@gmail.com")
+else:
+    st.title("Welcome to the Email Assistant")
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from safe_constants import SCOPES
+
+if "creds" not in st.session_state:
+  st.session_state.creds = None
+
+if "user_email" not in st.session_state:
+    st.session_state.user_email = None
+
+if "most_relevant_mails" not in st.session_state:
+  st.session_state.most_relevant_mails = None
+
+if "selected_mail" not in st.session_state:
+    st.session_state.selected_mail = None
+
+if "flow" not in st.session_state:
+    st.session_state.flow = None
+
+
+
+# Logout function
+def logout(is_from_login_func=False):
+    """Logs the user out by deleting the token and clearing session data."""
+    st.query_params.clear()
+
+    st.session_state.user_email = None
+    st.session_state.creds = None
+
+    if os.path.exists("token.json"):
+        os.remove("token.json")
+    if not is_from_login_func: st.success("Logged out successfully!")
+
+def login():
+    logout(is_from_login_func=True) # ensure clean slate before logging in
+    authorize_gmail_api()
+
+
+if st.query_params.get('code', None):
+    authenticate_user()
+
+
+
+
+
+
+if st.button("Login"):
+    login()
+
+if st.button("Logout"):
+    logout()
+    st.rerun()
+
+
+
+if st.button("Upload mail contents"):
+    st.info("While the app is in testing, only the latest 100 emails will be uploaded")
+    result_boolean = pinecone_utility.upload_email_content(index, user_email=st.session_state.user_email, max_emails=MAX_EMAILS)
+    if result_boolean: st.success("Emails uploaded successfully")
+
+
+st.write("## Query for specific emails (returns specific emails you are looking for)")
+prompt = st.text_input("Enter what emails you are looking for")
+
+
+col1, col2 = st.columns([1, 1])
+with col1:
+    if st.button("Get specific mails by content"):
+        if st.session_state.creds is None or st.session_state.user_email is None: st.error("Please login first")
+        elif prompt == "": st.error("Please enter a valid query")
+        else:
+            st.session_state.rag_response = None
+            mails = rag_agent.find_most_relevant_emails(prompt, top_k=K_MAILS_TO_RETURN)
+            if (mails and len(mails) > 0):
+                st.session_state.most_relevant_mails = mails
+                st.session_state.selected_mail = mails[0] # select the first mail by default (most similar)
+                st.session_state.selected_mail_index = 0
+
+                st.rerun()
+with col2:
+    if st.button("Ask general questions regarding emails"):
+        if st.session_state.creds is None or st.session_state.user_email is None: st.error("Please login first")
+        elif prompt == "": st.error("Please enter a valid query")
+        else:
+            response_text, mails = rag_agent.run_rag(prompt, K_MAILS_TO_RETURN)
+            if (response_text and len(response_text) > 0 and mails and len(mails) > 0):
+                st.session_state.rag_response = response_text
+                st.session_state.most_relevant_mails = mails
+                st.session_state.selected_mail = mails[0] # select the first mail by default (most similar)
+                st.session_state.selected_mail_index = 0
+
+                st.rerun()
+                
+
+
+
+# --------------------------------------------------------
+# General code
+# --------------------------------------------------------
+
+def render_mail(selected_mail):
+    email_subject = html.escape(selected_mail["subject"])
+    email_sender = html.escape(selected_mail["sender"])
+    email_date = html.escape(selected_mail["date"])
+    email_content = html.escape(selected_mail["text"])
+    email_link = selected_mail["email_link"]  # Get the email link from the selected_mail dictionary
+
+    # Custom CSS for styling the box and button
+    st.markdown(
+        """
+        <style>
+        .email-box {
+            background-color: #f0f9ff;
+            border: 1px solid #d1e7dd;
+            padding: 15px;
+            border-radius: 10px;
+            margin-bottom: 20px;
+        }
+        .email-content {
+            color: #333333;
+            font-size: 16px;
+            margin-top: 10px;
+        }
+        .email-header {
+            font-weight: bold;
+            font-size: 18px;
+            margin-bottom: 5px;
+        }
+        .email-subheader {
+            color: #6c757d;
+            font-size: 14px;
+            margin-bottom: 5px;
+        }
+        .email-link {
+            font-size: 16px;
+            margin-top: 20px;
+            display: inline-block;
+            background-color: #ff8c00; /* Orange background */
+            color: white !important; /* Ensure white text */
+            padding: 10px 20px;
+            text-align: center;
+            border-radius: 8px;
+            text-decoration: none;
+            transition: background-color 0.3s ease; /* Smooth transition */
+        }
+        .email-link:hover {
+            background-color: #e67600; /* Darker orange on hover */
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+    # Display the email inside a styled box with the clickable button link
+    st.markdown(f"""
+    <div class="email-box">
+        <div class="email-header">Subject: <span>{email_subject}</span></div>
+        <div class="email-subheader">From: <span>{email_sender}</span> | Date: <span>{email_date}</span></div>
+        <a href="{email_link}" target="_blank" class="email-link">Open Email in Gmail</a>
+        <div class="email-content"><span>{email_content}</span></div>
+    </div>
+    """, unsafe_allow_html=True)
+
+
+
+
+def update_selected_mail():
+    st.session_state.selected_mail = st.session_state.most_relevant_mails[st.session_state.selected_mail_index]
+
+if 'selected_mail_index' not in st.session_state:
+    st.session_state.selected_mail_index = 0  # Start at the most relevant email (index 0)
+
+def render_most_relevant_mails():
+    col1, col_mid, col2 = st.columns([1, 1, 1])
+    with col1:
+        if st.button("← Previous"):
+            if st.session_state.selected_mail_index == 0: st.warning("No more emails to show")
+            else:
+                st.session_state.selected_mail_index -= 1
+                update_selected_mail()
+
+    with col2:
+        if st.button("Next →"):
+            if st.session_state.selected_mail_index == len(st.session_state.most_relevant_mails) - 1: st.warning("No more emails to show")
+            else:
+                st.session_state.selected_mail_index += 1
+                update_selected_mail()
+
+    with col_mid:
+        st.write(f"Email {st.session_state.selected_mail_index + 1}/{len(st.session_state.most_relevant_mails)}")
+
+
+    # Update selected mail if not already set
+    if "selected_mail" not in st.session_state:
+        update_selected_mail()
+    
+    # Render the currently selected email
+    if st.session_state.selected_mail: render_mail(st.session_state.selected_mail)
+
+# --------------------------------------------------------
+# Full RAG to answer questions
+# --------------------------------------------------------
+
+if "rag_response" not in st.session_state:
+    st.session_state.rag_response = None
+
+if st.session_state.rag_response:
+    st.write("## RAG response")
+    st.write(st.session_state.rag_response)
+
+    # TODO render emails used to answer the question
+
+
+# --------------------------------------------------------
+# Display the most relevant emails
+# --------------------------------------------------------
+
+
+if st.session_state.most_relevant_mails is not None:
+    st.write("## Most relevant emails")
+    st.write(f"Emails listed in descending order of relevance for query: '{prompt}'")
+
+    render_most_relevant_mails()
+
+
+
+
+# Custom HTML for a green button
+button_html = """
+    <style>
+    .button {
+        background-color: #4CAF50; /* Green */
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        cursor: pointer;
+        border-radius: 12px;
+        margin: 20px 2px;
+        margin-top: 100px;
+            
+    }
+    </style>
+    <a href="https://buymeacoffee.com/kjosbakken" target="_blank">
+        <button class="button">Buy me a coffee</button>
+    </a>
+    """
+
+# Display the button
+st.markdown(button_html, unsafe_allow_html=True)
---
+++ utility.py
@@ -0,0 +1,132 @@
+import streamlit as st
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from openai import OpenAI
+from tqdm.auto import tqdm
+from pinecone import Pinecone
+import json
+
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+from dotenv import load_dotenv
+load_dotenv()
+from safe_constants import SCOPES, MAIN_REDIRECT_URI, ALL_REDIRECT_URIS, ALL_JAVASCRIPT_ORIGINS
+
+
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1' # avoids error being thrown for duplicate scopes (doesnt matter for this use case)
+
+# PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+
+pc = Pinecone(api_key=PINECONE_API_KEY)
+openai_client = OpenAI()
+
+
+from safe_constants import PROJECT_ID, AUTH_URI, TOKEN_URI, AUTH_PROVIDER_X509_CERT_URL
+CLIENT_ID = st.secrets["GMAIL_API_CREDENTIALS"]["CLIENT_ID"]
+CLIENT_SECRET = st.secrets["GMAIL_API_CREDENTIALS"]["CLIENT_SECRET"]
+
+CLIENT_CONFIG = {
+     "web":{"client_id":CLIENT_ID,"project_id":PROJECT_ID,"auth_uri":AUTH_URI,"token_uri":TOKEN_URI,"auth_provider_x509_cert_url":AUTH_PROVIDER_X509_CERT_URL,"client_secret":CLIENT_SECRET,"redirect_uris": ALL_REDIRECT_URIS,"javascript_origins": ALL_JAVASCRIPT_ORIGINS}
+     }
+
+
+def get_user_info(creds):
+    # Build the OAuth2 service to get user info
+    oauth2_service = build('oauth2', 'v2', credentials=creds)
+    
+    # Get user info
+    user_info = oauth2_service.userinfo().get().execute()
+
+    return user_info.get('email')
+
+
+def authorize_gmail_api():
+      """Shows basic usage of the Gmail API.
+      Lists the user's Gmail labels.
+      """
+      creds = None
+      if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+        st.info("Already logged in")
+      # If there are no (valid) credentials available, let the user log in.
+      if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+          creds.refresh(Request())
+        else:
+          flow = InstalledAppFlow.from_client_config(
+              CLIENT_CONFIG, SCOPES
+          )
+          flow.redirect_uri = MAIN_REDIRECT_URI
+
+          authorization_url, state = flow.authorization_url(
+              access_type='offline',
+              include_granted_scopes='true',
+              prompt='consent')
+
+
+          st.markdown(
+            f"""
+            <style>
+            .custom-button {{
+                display: inline-block;
+                background-color: #4CAF50; /* Green background */
+                color: white !important;  /* White text */
+                padding: 10px 24px;
+                text-align: center;
+                text-decoration: none;
+                font-size: 16px;
+                border-radius: 5px;
+                margin-top: 5px; /* Reduce space above the button */
+                margin-bottom: 5px; /* Reduce space below the button */
+            }}
+            .custom-button:hover {{
+                background-color: #45a049;
+            }}
+            </style>
+            <a href="{authorization_url}" target="_blank" class="custom-button">Authorize with Google</a>
+            """,
+            unsafe_allow_html=True
+        )
+            
+
+def authenticate_user():
+    """after loggin in with google, you have a code in the url. This function retrieves the code and fetches the credentials and authenticates user"""
+    auth_code = st.query_params.get('code', None)
+    if auth_code is not None:
+        logger.info("INSIDE CODE")
+        from utility import CLIENT_CONFIG
+        
+        # make a new flow to fetch tokens
+        flow = InstalledAppFlow.from_client_config(
+                CLIENT_CONFIG, SCOPES, 
+            )
+        flow.redirect_uri = MAIN_REDIRECT_URI
+        flow.fetch_token(code=auth_code)
+        st.query_params.clear()
+        creds = flow.credentials
+        if creds:
+            st.session_state.creds = creds
+            # Save the credentials for future use
+            with open('token.json', 'w') as token_file:
+                token_file.write(creds.to_json())
+            st.success("Authorization successful! Credentials have been saved.")
+
+            # Save the credentials for the next run
+            with open("token.json", "w") as token: 
+                token.write(creds.to_json())
+            # get user email
+            user_email = get_user_info(creds)
+            st.session_state.user_email = user_email
+            st.rerun()
+    else: st.error("Could not log in user")
