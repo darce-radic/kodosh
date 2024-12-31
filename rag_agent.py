855a5f3e549e2b21ae58b37ee603515607b3384a
darce-radic <132355796+darce-radic@users.noreply.github.com>
2025-01-01 02:25:10 +1100

    Update rag_agent.py

--- rag_agent.py
+++ rag_agent.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os  # Add this import statement
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
